1c20ca3375593ea0dbd8f4c90222b965f43846ec
rama <rama2kalloub@gmail.com>
2026-06-04 18:21:30 +0300

    added the localization library for changing the language

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get appTitle => 'Invoice AI';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get language => 'Language';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Invoice AI';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get language => 'Language';
+}
